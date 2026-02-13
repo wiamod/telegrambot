c656e7892e1171c1c8366e8f71ceb15254aa5499
wiamod <eshmatteshaboyev@gmail.com>
2026-02-13 11:36:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,29 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+const token = process.env.TOKEN;
+const bot = new TelegramBot(token);
+
+const PORT = process.env.PORT || 3000;
+const url = process.env.RAILWAY_STATIC_URL || process.env.RAILWAY_PUBLIC_DOMAIN;
+
+// webhook
+bot.setWebHook(`https://${url}/bot${token}`);
+
+app.use(express.json());
+
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
 
 app.get('/', (req, res) => {
   res.send('Bot ishlayapti 🚀');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log('Server ready');
-});
-
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, 'Salom! Railwayda 24/7 ishlayapman 🚀');
 });
+
+app.listen(PORT, () => console.log('Server ready'));
